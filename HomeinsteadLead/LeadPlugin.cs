b34b59f201cc00219bef761e1964dfc8d41ba970
dmgreeneus <dmgreeneus@gmail.com>
2016-08-25 14:50:15 -0500

    New Plugin for Homeinstead Leads

---
+++ HomeinsteadLead/LeadPlugin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+
+
+namespace HomeinsteadLead
+{
+    public class LeadPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+               (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+            {
+                Entity lead = (Entity)context.InputParameters["Target"];
+                if (lead.LogicalName != "lead")
+                    return;
+                String statevalue = null;
+                try
+                {
+                    if (lead.Attributes.Contains("hisc_state"))                   {
+                        statevalue = lead.GetAttributeValue<String>("hisc_state");
+
+                        Entity state = new Entity();
+                        state.LogicalName = "hisc_state";
+                        QueryExpression query = new QueryExpression
+                        {
+                            EntityName = state.LogicalName,
+                            ColumnSet = new ColumnSet("hisc_stateid"),
+                            Criteria = new FilterExpression
+                            {
+                                FilterOperator = LogicalOperator.And,
+                                Conditions =
+                            {
+                                new ConditionExpression
+                                {
+                                   AttributeName = "hisc_name",
+                                   Operator = ConditionOperator.Equal,
+                                   Values = { statevalue }
+                                }
+                            }
+                            }
+                        };
+                        EntityCollection results = service.RetrieveMultiple(query);
+                       
+
+                        foreach (var result in results.Entities)
+                        {
+                            Guid stateid = result.GetAttributeValue<Guid>("hisc_stateid");
+                            lead.Attributes["hisc_stateprovinceid"] = new EntityReference(state.LogicalName,stateid);
+                            
+                        }
+                        
+                    }
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("an error occured in lead plugin", ex);
+                }
+                catch (Exception ex)
+                {
+                    tracingService.Trace("HISC_Lead_Pluign: {0}", ex.Message.ToString());
+                    throw;
+
+                }
+
+            }
+        }
+    }
+}
+
